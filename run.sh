78b52e00f3923b1323c92c9ae5dcc278d8bbca6e
lcaballero <caballero.luke@gmail.com>
2023-06-28 17:20:03 -0600

    Fixing badges so that they aren't in p tags

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DIR="$(cd "/Users/lucas/dotfiles/bin" >/dev/null 2>&1 && pwd)"
-
 build() {
     go install
 }
@@ -13,6 +11,28 @@ tests() {
 execute() {
     echo "execute not implemented yet"
     exit 1
+}
+
+badges::imgs() {
+    (
+
+      cat <<EOF
+<img alt="GitHub Workflow Action Status"
+     src="https://github.com/lcaballero/codefmt/actions/workflows/main.yaml/badge.svg"/>
+<img alt="Go Report codefmt (this repo)"
+     src="https://goreportcard.com/badge/github.com/lcaballero/codefmt"/>
+EOF
+
+    ) | \
+      sed -E 's/  +//g' | tr '\n' ' ' | sed -E 's/> </></g'
+
+}
+
+badges() {
+    echo "#+begin_html"
+    badges::imgs
+    echo
+    echo "#+end_html"
 }
 
 "$@"
